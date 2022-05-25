2dbe3fd1e71aad840b92428d3f3a19aa380aa742
Milton Calnek <milton@calnek.com>
2022-05-25 00:00:13 -0600

    immediate roadmap.

--- urlReview2.pl
+++ urlReview2.pl
@@ -3,6 +3,7 @@
 
 my @artifacts;
 push @artifacts, q(http://508ma.com/zc/cqwmall?user=patty.niebergall@uregina.ca);
+# read from argv[0]
 
 
 sub analyze_artifact {
@@ -18,24 +19,44 @@ sub analyze_url {
 
 	print "Analyzing url: $art\n";
 	my $newart = q(https://508ma.com/zc/cqwmall?user=patty.niebergall@uregina.ca);
-	my $dom = q(508ma.com);
-	my $ip = q(68.65.122.222);
+	my $dom = q(508ma.com); # isolate host name
+	my $ip = q(68.65.122.222); # ip of host
 
 	push @artifacts, $dom unless grep({ $dom eq $_ } @artifacts);
 	push @artifacts, $ip unless grep({ $ip eq $_ } @artifacts);
 	push @artifacts, $newart unless grep({ $newart eq $_ } @artifacts);
+	# check url against VT
+	# check url against misp
+	# check url against cyber gordon
+	# is this the last redirection?
+	# if yes:
+	#   check for word press
+	#   check for joomla
+	#   check for cpanel
+	#   check for security contact
+	#   compare output against browser variations
+	# if no:
+	#   add new location to artifacts
 }
 
 sub analyze_ip {
 	my ($art) = @_;
 
 	print "Analyzing ip: $art\n";
+	# check ip against VT
+	# check ip against misp
+	# check ip against cyber gordon
 }
 
 sub analyze_name {
 	my ($art) = @_;
 
 	print "Analyzing name: $art\n";
+	# check name against VT
+	# check name against misp
+	# check name against cyber gordon
+	# check for http[s]://name/contact
+
 }
 
 
